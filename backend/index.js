21ec8f8bc952fc89bd9e26b4e65b5d5e72cd73e7
Abhishek Kumar <abhishekwork000111@gmail.com>
2026-09-07 11:34:02 +0530

    connect to homePage

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const url = process.env.MONGO_URL;
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://stock-website-home.netlify.app/",
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
