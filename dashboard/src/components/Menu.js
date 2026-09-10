a830a0622a2676e362574c804f5111ba5e5c08da
Abhishek Kumar <abhishekwork000111@gmail.com>
2026-09-10 11:52:13 +0530

    Added bootstrapCSS

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -122,15 +122,26 @@ const Menu = () => {
           </li>
         </ul>
         <hr />
-        <div className="dropdown" onClick={handleProfileClick}>
-          <div className="avatar">
-            {username ? username.slice(0, 2).toUpperCase() : "ZU"}
-          </div>
-          <p className="dropdown-menu btn btn-secondary dropdown-toggle">{username || "Guest"}</p>
-          {isProfileDropdownOpen && (
-            <div className="dropdown-item">
-              <button onClick={handleLogout}>Logout</button>
+        <div className="dropdown">
+          <div
+            className="d-flex align-items-center btn btn-secondary dropdown-toggle"
+            onClick={handleProfileClick}
+            role="button"
+          >
+            <div className="avatar me-2">
+              {username ? username.slice(0, 2).toUpperCase() : "ZU"}
             </div>
+            <span>{username || "Guest"}</span>
+          </div>
+
+          {isProfileDropdownOpen && (
+            <ul className="dropdown-menu show">
+              <li>
+                <button className="dropdown-item" onClick={handleLogout}>
+                  Logout
+                </button>
+              </li>
+            </ul>
           )}
         </div>
       </div>
