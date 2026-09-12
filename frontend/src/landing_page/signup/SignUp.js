5804b2dafa6f31ddc212abd980cc3a746245175d
Abhishek Kumar <abhishekwork000111@gmail.com>
2026-09-12 18:29:50 +0530

    Debugging

--- frontend/src/landing_page/signup/SignUp.js
+++ frontend/src/landing_page/signup/SignUp.js
@@ -34,6 +34,7 @@ const Signup = () => {
       const { success, message, username } = data;
       if (success) {
         handleSuccess(`Welcome, ${username}!`); // ✅ Fix 4: show username on success
+        console.log("Signup response:",data);
         setInputValue({ email: "", password: "", username: "" }); // ✅ Fix 3: reset only on success
         setTimeout(() => {
              window.location.href = "https://stock-website-dashboard.netlify.app/";
